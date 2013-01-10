b153ce47206a6601135a21e32686e27ed21b9afa
Derek <heyderek@dereknelson.net>
2013-01-10 14:24:42 -0800

    Added js support for destinations.

--- js/custom.js
+++ js/custom.js
@@ -2,12 +2,6 @@ $(document).ready(function(){
   $('ul.destination-list').hide();
   $('.learnmore').click(function(){
     $(this).next('ul.destination-list').slideDown('fast').addClass('active');
-    if($(this).next('ul.destination-list').hasClass('active')){
-      console.log('here:',$(this));
-      $(this).next('ul');
-    }else{
-      
-    }
     $('.close').click(function(){
       $(this).parent().slideUp('fast').removeClass('active');
     });
