d8fcc080c60b100636be049a11e602fc6af782a7
Derek <heyderek@dereknelson.net>
2013-01-10 11:33:16 -0800

    Added more support for destinations.

--- taxonomy-region.php
+++ taxonomy-region.php
@@ -11,15 +11,34 @@
               </p>
           </header>
           <?php
-          
+          $taxonomy = 'region';
           $region = $wp_query->queried_object_id;
-          $children = get_term_children( $region, 'region' );
+          $children = get_term_children( $region, $taxonomy );
           
           foreach($children as $child) :
           
-            $term = get_term_by( 'id', $child, 'region' );
+            $term = get_term_by( 'id', $child, $taxonomy );
+            $slug = $term->slug;
+            $args = array(
+              'posts_per_page' => 1,
+              'tax_query' => array(
+                array(
+                  'taxonomy' => $taxonomy,
+                  'field' => 'slug',
+                  'terms' => $slug
+                )
+              )
+            );
+            $posts = new WP_Query($args);
             
-            echo $term->name;
+            /* echo '<h4>' . $term->name . '</h4>'; */
+            
+            while($posts->have_posts()) : $posts->the_post();
+              
+              echo '<h4>' . $term->name . '</h4>';
+              echo the_post_thumbnail('featured');
+              
+            endwhile;
             
           endforeach;
           ?>
